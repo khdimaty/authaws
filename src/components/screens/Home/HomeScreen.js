ce5887528d545a3f9a84b43968ea39c38b696785
khdimaty <anasaitaomar1999@gmail.com>
2019-11-06 20:11:30 +0100

    change style-homescreen -towardxd

--- src/components/screens/Home/HomeScreen.js
+++ src/components/screens/Home/HomeScreen.js
@@ -5,7 +5,7 @@ import {
   TouchableOpacity,
   RefreshControl
 } from "react-native";
-import Amplify, { Storage } from "@aws-amplify/core";
+//import Amplify, { Storage } from "@aws-amplify/core";
 import { Container, Header, Text, Spinner } from "native-base";
 import Modal from "react-native-modal";
 import { Query } from "@apollo/react-components";
@@ -14,8 +14,10 @@ import { Dimensions } from "react-native";
 import Ascard from "./components/appstorecard";
 import Mymodal from "./components/modal";
 import { ScrollView } from "react-native-gesture-handler";
-import { Accordion, Badge, Button } from "native-base";
+import { BlurView } from "expo-blur";
 import { TouchableHighlight } from "react-native";
+import Constants from "expo-constants";
+import { Icon } from "react-native-elements";
 const Tasks = gql`
   {
     tasks(orderBy: name_ASC) {
@@ -58,42 +60,11 @@ export default class HomeScreen extends React.Component {
     });
   };
   onRefresh() {}
-  state = { activea: 1, activeb: 0, activec: 0 };
+  state = { activea: "white", activeb: "black", activec: "black" };
   render() {
     let { activea, activeb, activec } = this.state;
     return (
-      <Container>
-        <Header style={{ backgroundColor: "#fff", alignItems: "center" }}>
-          <Text style={{ fontSize: 30, fontWeight: "bold" }}>Khdimaty</Text>
-        </Header>
-
-        <View style={styles.Tabs}>
-          <TouchableOpacity
-            style={[styles.nav, { borderWidth: activea }]}
-            onPress={() =>
-              this.setState({ activea: 1, activeb: 0, activec: 0 })
-            }
-          >
-            <Text style={styles.text}>Following</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.nav, { borderWidth: activeb }]}
-            onPress={() =>
-              this.setState({ activea: 0, activeb: 1, activec: 0 })
-            }
-          >
-            <Text style={styles.text}>Popular</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.nav, { borderWidth: activec }]}
-            onPress={() =>
-              this.setState({ activea: 0, activeb: 0, activec: 1 })
-            }
-          >
-            <Text style={styles.text}>Explore</Text>
-          </TouchableOpacity>
-        </View>
-
+      <View style={styles.container}>
         <Query query={Tasks}>
           {({ loading, error, data, refetch, networkStatus }) => {
             if (loading) return <Spinner color="blue" />;
@@ -101,6 +72,7 @@ export default class HomeScreen extends React.Component {
 
             return (
               <ScrollView
+                style={{ paddingTop: 70 }}
                 refreshControl={
                   <RefreshControl
                     //refresh control used for the Pull to Refresh
@@ -116,25 +88,79 @@ export default class HomeScreen extends React.Component {
                   />
                 }
               >
-                {data.tasks.map(task => (
-                  <TouchableHighlight
-                    onPress={() => this.toTask(task)}
-                    underlayColor="white"
-                    key={task.id}
+                <View style={styles.Tabs}>
+                  <TouchableOpacity
+                    style={[styles.nav]}
+                    onPress={() =>
+                      this.setState({
+                        activea: "white",
+                        activeb: "black",
+                        activec: "black"
+                      })
+                    }
                   >
-                    <Ascard
-                      name={task.name}
-                      score={task.taskScore}
-                      type={task.type}
-                      navigation={this.props.navigation}
-                      modal={this.toggleModal}
-                    />
-                  </TouchableHighlight>
-                ))}
+                    <Text style={[styles.text, { color: activea }]}>
+                      Newest
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={[styles.nav]}
+                    onPress={() =>
+                      this.setState({
+                        activea: "black",
+                        activeb: "white",
+                        activec: "black"
+                      })
+                    }
+                  >
+                    <Text style={[styles.text, { color: activeb }]}>
+                      Popular
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={[styles.nav]}
+                    onPress={() =>
+                      this.setState({
+                        activea: "black",
+                        activeb: "black",
+                        activec: "white"
+                      })
+                    }
+                  >
+                    <Text style={[styles.text, { color: activec }]}>
+                      Favorite
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View>
+                  {data.tasks.map(task => (
+                    <TouchableHighlight
+                      onPress={() => this.toTask(task)}
+                      underlayColor="white"
+                      key={task.id}
+                    >
+                      <Ascard
+                        name={task.name}
+                        score={task.taskScore}
+                        type={task.type}
+                        navigation={this.props.navigation}
+                        modal={this.toggleModal}
+                      />
+                    </TouchableHighlight>
+                  ))}
+                </View>
               </ScrollView>
             );
           }}
         </Query>
+        <BlurView tint="default" intensity={100} style={styles.static}>
+          <View style={styles.header}>
+            <Icon name="tram" size={35} />
+
+            <Text style={{ fontSize: 35 }}>Khdimaty</Text>
+            <Icon name="search" size={35} />
+          </View>
+        </BlurView>
 
         <Modal
           isVisible={this.state.isModalVisible}
@@ -157,22 +183,44 @@ export default class HomeScreen extends React.Component {
             type={this.state.type}
           />
         </Modal>
-      </Container>
+      </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1
-    // backgroundColor: "#FFF"
+    // backgroundColor: "#ee1",
+
+    // marginTop: 15
+  },
+  static: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    backgroundColor: "#fff"
+  },
+  header: {
+    alignItems: "center",
+    //backgroundColor: "#ee9",
+    marginTop: 20,
+    padding: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around"
   },
   Tabs: {
     height: 70,
+    backgroundColor: "#3c3c3c",
+
+    marginHorizontal: 15,
+    marginVertical: 30,
 
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
-    elevation: 0
+    borderRadius: 30
   },
   nav: {
     width: 100,
@@ -188,5 +236,9 @@ const styles = StyleSheet.create({
   text: {
     color: "#000",
     alignSelf: "center"
+  },
+  notBlurred: {
+    ...StyleSheet.absoluteFill,
+    top: Constants.statusBarHeight
   }
 });
